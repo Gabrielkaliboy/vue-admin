b29d5d80533d628da7d32ad7b5a3c53691449783
Gabriel_dell <Gabriel_wei@163.com>
2019-04-15 22:30:45 +0800

    两个组建之间传值

--- src/components/AInput.vue
+++ src/components/AInput.vue
@@ -13,6 +13,8 @@ export default {
     methods: {
         handleInput(event){
             const value = event.target.value;
+            // 子组件要想修改父组件的值要以参数的形式放在这里，以事件发出去
+            // 父组件绑定一个事件来知道子组件要改一个值，改值的操作在子组建里修改
             this.$emit('input',value)
         }
     }
--- src/views/store.vue
+++ src/views/store.vue
@@ -1,14 +1,20 @@
 <template>
     <div>
-        <AInput v-model = "inputValue"></AInput>
-        {{ inputValue }}
+        <!-- <AInput v-model = "inputValue"></AInput> -->
+        <!-- 相当于 -->
+        <!-- <a-input :value='inputValue' @input = "handleInput"></a-input> -->
+        <a-input @input = "handleInput"></a-input>
+        <p>{{ inputValue }} </p>
+        <a-show :content='inputValue'></a-show>
     </div>
 </template>
-<script>
+<script> 
 // import AInput from '../components/AInput'
 import AInput from '@/components/AInput' 
 //  <a-input></a-input>或者<AInput></AInput>
 // import AInput from '_c/AInput' 不知道这个路由为什么匹配不上
+
+import AShow from '@/components/AShow'
 export default {
     name:'store',
     data(){
@@ -17,7 +23,19 @@ export default {
         }
     },
     components:{
-        AInput:AInput
+        AInput:AInput,
+        AShow,
+    },
+    //不用v-model方法就要自己写了
+    // methods:{
+    //     handleInput(val){
+    //         this.inputValue = val
+    //     }
+    // }
+    methods:{
+        handleInput(val){
+            this.inputValue = val
+        }
     }
 }
 </script>
